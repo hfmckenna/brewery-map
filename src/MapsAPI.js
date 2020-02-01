a008373fff0a6ee431c50b8b7755dce052acd7d5
Hugh McKenna <hfmckenna@outlook.com>
2020-02-01 18:14:59 +0000

    Chore change to reset API key

--- src/MapsAPI.js
+++ src/MapsAPI.js
@@ -1,6 +1,6 @@
 /* My Google maps API key and the URL for use with react-google-maps withScript in Map.js */
 
-const myKey = 'AIzaSyBhN6IwT717vqkQRYXaQb72bVd8cfgWKF0'
+const myKey = 'AIzaSyBOahgd7s23Z0Zmye5QNMC6ukxhs2wDpJo'
 export const googleURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${myKey}`
 
 /* Styles for react-google-maps stored here as have no intention of making it dynamic, so keeps things separated*/
